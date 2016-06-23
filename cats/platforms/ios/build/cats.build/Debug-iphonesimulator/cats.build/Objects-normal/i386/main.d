8e3700baba08352055f81aa9478725e3d975586a
sophialazarova <sophia.lazarova@yahoo.com>
2016-06-23 08:52:34 +0300

    platform folder removed

--- cats/platforms/ios/build/cats.build/Debug-iphonesimulator/cats.build/Objects-normal/i386/main.d
+++
@@ -1,7 +0,0 @@
-dependencies: /Users/sophia/Cats/cats/platforms/ios/internal/main.m \
-  /Users/sophia/Cats/cats/platforms/ios/internal/NativeScript/include/NativeScript.h \
-  /Users/sophia/Cats/cats/platforms/ios/internal/NativeScript/include/TNSRuntime.h \
-  /Users/sophia/Cats/cats/platforms/ios/internal/NativeScript/include/TNSRuntime+Diagnostics.h \
-  /Users/sophia/Cats/cats/platforms/ios/internal/NativeScript/include/TNSRuntime+Inspector.h \
-  /Users/sophia/Cats/cats/platforms/ios/internal/TNSExceptionHandler.h \
-  /Users/sophia/Cats/cats/platforms/ios/internal/TNSDebugging.h
--- cats/platforms/ios/build/emulator/cats.app/app/details.js
+++
@@ -1,8 +0,0 @@
-var observable = require('data/observable');
-var bindingContext = new observable.Observable();
-
-exports.onLoaded = function(args){
-    var page = args.object;
-    bindingContext = page.navigationContext;
-    page.bindingContext = bindingContext;
-};
